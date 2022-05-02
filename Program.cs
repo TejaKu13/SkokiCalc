eb9d7634fbf199309d1dbbbba105e7058d797664
Marlena Kurczab <mojca13@gazeta.pl>
2022-05-02 22:03:35 +0200

    część 2.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkokiCalc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skoki,ach te skoki...");
+        }
+    }
+}
---
+++ PunktySkoku.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkokiCalc
+{
+    public class PunktySkoku
+    {
+        public double punktK;
+        public double punktyZaMetr;
+
+
+    }
+}
